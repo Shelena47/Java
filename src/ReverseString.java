cb665836a5976374f51a0322992b7b1be8574729
Shelena47 <127796327+Shelena47@users.noreply.github.com>
2023-04-22 00:45:04 +0100

    Update ReverseString.java

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 public class ReverseString {
     public static void main(String[] args) {
         System.out.println("This program will reverse any word--");
-
+        //create scanner object
         Scanner rs = new Scanner(System.in);
+        //read user input as char
         System.out.println("Enter any word: ");
         char[] letters = rs.nextLine().toCharArray();
         for (int i = letters.length -1; i >= 0; i--) {
